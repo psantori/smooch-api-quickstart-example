e8870a40caf164be74666b66e690abf39f4b1fb7
Frederick <frederick.duberville@gmail.com>
2017-02-21 17:10:36 -0500

    initial commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,50 +1,82 @@
-'use strict';
+////////////// Easy way
+var dotenv = require('dotenv');
+dotenv.config();
+dotenv.load();
+var slack_oauth = require('./slackoauth')
 
-// Imports
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+var bot_token = process.env.SLACK_BOT_TOKEN;
+console.log(bot_token)
+//console.log(slack_oauth.app)
+var rtm = new RtmClient(bot_token);
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+// you need to wait for the client to fully connect before you can send messages
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+  //rtm.sendMessage("Hello!", 'C3U3BMP1V');
+});
+
+rtm.start();
+
+// Initialize using verification token from environment variables
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event)=> {
+  const attachments = event && event.attachments || [];
+  console.log(event);
+  for (attachment of attachments){
+    if(attachment.pretext == 'hey'){
+      rtm.sendMessage(`<#${event.channel}>`, 'C3U3BMP1V');
+    }
+  }
+  //rtm.sendMessage("Hello!", 'C3U3BMP1V');
+  //console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+  //console.log(event);
+});
+
+// Start a basic HTTP server
+// slackEvents.start(port).then(() => {
+//   console.log(`server listening on port ${port}`);
+// });
+
+/////// Express wey
+
+const http = require('http');
+
+// Initialize using verification token from environment variables
+//const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+//const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+
+// Initialize an Express application
 const express = require('express');
 const bodyParser = require('body-parser');
-const Smooch = require('smooch-core');
+//const app = express();
 
-// Config
-const PORT = 8000;
-const KEY_ID = 'your_key_id';
-const SECRET = 'your_secret_key';
+// You must use a body parser for JSON before mounting the adapter
+slack_oauth.app.use(bodyParser.json());
 
-const smooch = new Smooch({
-    keyId: KEY_ID,
-    secret: SECRET,
-    scope: 'app'
-});
+// Mount the event handler on a route
+// NOTE: you must mount to a path that matches the Request URL that was configured earlier
+slack_oauth.app.use('/event', slackEvents.expressMiddleware());
 
-// Server https://expressjs.com/en/guide/routing.html
-const app = express();
 
-app.use(bodyParser.json());
+// Start the express application
+// http.createServer(slack_oauth.app).listen(process.env.PORT, () => {
+//   console.log(`server listening on port ${port}`);
+// });
 
-// Expose /messages endpoint to capture webhooks https://docs.smooch.io/rest/#webhooks-payload
-app.post('/message', function(req, res) {
-  console.log('webhook PAYLOAD:\n', JSON.stringify(req.body, null, 4));
-
-  const appUserId = req.body.appUser._id;
-  // Call REST API to send message https://docs.smooch.io/rest/#post-message
-  if (req.body.trigger === 'message:appUser') {
-      smooch.appUsers.sendMessage(appUserId, {
-          type: 'text',
-          text: 'Live long and prosper',
-          role: 'appMaker'
-      })
-          .then((response) => {
-              console.log('API RESPONSE:\n', response);
-              res.end();
-          })
-          .catch((err) => {
-              console.log('API ERROR:\n', err);
-              res.end();
-          });
-  }
-});
-
-// Listen on port
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+// // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+// slackEvents.on('message', (event)=> {
+//   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+// });
---
+++ nodejs/slackoauth.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var request = require('request');
+var dotenv = require('dotenv');
+dotenv.config();
+dotenv.load();
+
+// Slack IDs
+var clientId = process.env.SLACK_CLIENT_ID;
+var clientSecret = process.env.SLACK_CLIENT_SECRET;
+
+var app = exports.app = express();
+const PORT=process.env.PORT;
+
+app.listen(PORT, function () {
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Example app listening on port " + PORT);
+});
+
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+    res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+    // When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // If it's there...
+
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+            method: 'GET', //Specify the method
+
+        }, function (error, response, body) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(body);
+
+            }
+        })
+    }
+
+    // Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+    app.post('/command', function(req, res) {
+        res.send('Your ngrok tunnel is up and running!');
+    });
+});
